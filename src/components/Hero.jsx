6618dc7193a6162ec4f7e49b93d3efc66efb6ecc
Eduardo <eduardo.hpa87@gmail.com>
2023-09-21 12:30:33 -0300

    feat: renders new button to scroll down to about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
@@ -40,6 +41,45 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div
+        className="
+        absolute 
+        xs:bottom-10 
+        bottom-32 
+        w-full 
+        flex 
+        justify-center 
+        items-centers"
+      >
+        <Link to="about">
+          <div
+            className="
+              w-[35px]
+              h-[64px]
+              rounded-3xl
+              border-4
+              border-secondary
+              flex
+              justify
+              items-start
+              p-2
+            "
+          >
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </Link>
+      </div>
     </section>
   );
 };
